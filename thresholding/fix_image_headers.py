acb174d8c076c1dc91c449c4005c25f68543b3dc
poldrack <poldrack@gmail.com>
2015-04-26 15:22:34 -0700

    finished first version, waiting for VN results

--- thresholding/fix_image_headers.py
+++ thresholding/fix_image_headers.py
@@ -1,6 +1,6 @@
 import glob
 
-images=glob.glob('data/randomize*/*')
+images=glob.glob('data/randomize*/*nii.gz')+glob.glob('data/*nii.gz')
 
 for i in images:
     cmd='fslcpgeom data/bg_image.nii.gz %s'%i
