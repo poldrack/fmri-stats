0a92a855dbf485b9cb540d4b9a0d7ee0c01fbc4d
poldrack <poldrack@gmail.com>
2015-04-26 17:07:34 -0700

    adding additional files, changed name of notebook

--- thresholding/mk_fdr_tmaps.py
+++ thresholding/mk_fdr_tmaps.py
@@ -2,3 +2,7 @@ nvals=[25,50,75,100,125]
 for n in nvals:
     cmd='fslmaths data/fdr_1minusp_%dsubs.nii.gz -mul data/tt_%dsubs_tstat1.nii.gz data/fdr_tstat_%dsubs.nii.gz'%(n,n,n)
     print cmd
+    
+for n in [25,50]:
+    cmd='fslmaths data/fdr_1minusp_%dsubs_set2.nii.gz -mul data/tt_%dsubs_set2_tstat1.nii.gz data/fdr_tstat_%dsubs_set2.nii.gz'%(n,n,n)
+    print cmd
